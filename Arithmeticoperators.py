46b5ae9ab17e1e3c04278fa689f0cb4ddcf756f1
ribagithub <152161244+ribagithub@users.noreply.github.com>
2023-11-27 18:19:43 +0530

    Add files via upload

---
+++ Arithmeticoperators.py
@@ -0,0 +1,12 @@
+a=int(input("enter 1st value"))
+b=int(input("enter 2nd value"))
+c=a+b              #(+)addition operator
+print("add=",c)     
+c=a-b              #(-)minus operator
+print("sub=",c)
+c=a*b              #(*)multipication operator
+print("mul=",c)
+c=a/b              #(/)division operator
+print("div=",c)
+c=a%b              #(%)modulo opertor[returns the value of remainder after division]
+print("mod=",c)
